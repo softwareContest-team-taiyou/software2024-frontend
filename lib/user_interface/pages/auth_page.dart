67191a7c2a79ca3086410b5ec805974ac191e818
kimura <taiyo141130@gmail.com>
2024-10-23 11:26:49 +0900

    ログイン画面を作成　画像を２つ貼ったのでpubspec.yamlにも変更ありassetファイル作成した

--- lib/user_interface/pages/auth_page.dart
+++ lib/user_interface/pages/auth_page.dart
@@ -11,17 +11,66 @@ class MainView extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        ElevatedButton(
-            onPressed: () async {
-              // Use a Universal Link callback URL on iOS 17.4+ / macOS 14.4+
-              // useHTTPS is ignored on Android
-              login(ref);
-            },
-            child: const Text('ログイン'))
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'BoxKeyPer',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Color(0xFFF7F7F7),
+          ),
+        ),
+        backgroundColor: const Color(0xFF38A5C6), // AppBarの背景色
+      ),
+      backgroundColor: const Color(0xFFFFFFFF),
+      body: Center(
+        child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            //ログインアイコン
+            Image.asset('assets/images/login_icon.jpg', // ローカルの画像
+              width:450,
+              height:450
+            ),
+
+            //ログインボタン
+            Positioned(
+              bottom: 100, 
+              child: ElevatedButton(
+                onPressed: () async {
+                  login(ref);
+                },
+                child: const Text(
+                  'Login',
+                  style: TextStyle(
+                    fontSize: 24, // テキストのフォントサイズ
+                    fontWeight: FontWeight.bold, // テキストを太字に
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFF479955), // ボタンの背景色を変更
+                  foregroundColor: Colors.white, // ボタンのテキスト色を変更
+                  padding: EdgeInsets.symmetric(horizontal: 32, vertical: 16), // ボタンのパディングを増やす
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8), // ボタンの角を丸くする
+                  ),
+                ),
+              ),
+            ),
+            //カエルキャラクター
+            Positioned(
+              right: 10, // 画面右から10ピクセルの位置
+              bottom: 10, // 画面下から10ピクセルの位置
+              child: Image.asset(
+                'assets/images/software_character.jpg', // キャラクター画像
+                width: 100, // キャラクター画像の幅
+                height: 100, // キャラクター画像の高さ
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
